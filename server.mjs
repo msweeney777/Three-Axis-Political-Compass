d86079ca6dc2281c8bcd14bd00e1cc6ae6a881e0
Matt Sweeney <msweeney777@gmail.com>
2019-01-18 18:35:43 -0500

    Added questions.mjs to mix

--- server.mjs
+++ server.mjs
@@ -5,9 +5,12 @@ const app = express();
 import home from './app/index.mjs';
 import contact from './app/views/contact.mjs';
 
+import plot from './app/api/plot.mjs' 
+
 app.get('/', home);
 app.get('/contact', contact);
 
+app.post('api/plot.mjs', plot);
 
 app.listen(3000, () => {
   console.log("Server up and running.")
